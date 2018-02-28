c682fc843c020c8b0e3106e201e155f3a97408d5
Kallol BIkash Baidya <kallol.baidya@pilottravelcenters.com>
2018-02-28 11:24:17 +0530

    chnages config and access modifier

--- frontend/app/Components/pfj-data-tile/pfj-data-tile.component.ts
+++ frontend/app/Components/pfj-data-tile/pfj-data-tile.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PfjDataTileComponent implements OnInit {
   @Input() dataTileName : string;
-  @Input() dataTile : object;
+  @Input() dataTile : any;
   @Input() isRequiredSubTile:boolean;
   @Input() arrItemsSubTile
   constructor() { }
