8c1c2a4d5e23a926c87ae253fc35a7a74c26c640
HoneyBhatnagarPilot <36271484+HoneyBhatnagarPilot@users.noreply.github.com>
2018-03-03 15:43:37 -0500

    Updated Angular App Config

--- frontend/app/app-config.ts
+++ frontend/app/app-config.ts
@@ -1,5 +1,5 @@
 export class AppConfig
 {
     //static PFJApiUrl:string="https://pilot-fpp-dev.herokuapp.com/";
-    static PFJApiUrl:string="http://http://fuel-pricing-platform-dev.herokuapp.com/";
+    static PFJApiUrl:string="http://fuel-pricing-platform-dev.herokuapp.com/";
 }
