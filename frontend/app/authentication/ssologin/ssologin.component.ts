9adbc8657865c42ff30dd0893c70c9804573f339
Kallol <kallol.baidya@pilottravelcenters.com>
2018-03-16 18:06:47 +0530

    change

--- frontend/app/authentication/ssologin/ssologin.component.ts
+++ frontend/app/authentication/ssologin/ssologin.component.ts
@@ -10,7 +10,7 @@ export class SSOLoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    location.href = 'http://spring-saml-sample.herokuapp.com';
+    location.href = 'https://spring-saml-sample.herokuapp.com/saml/discovery?entityID=https%3A%2F%2Fspring-saml-sample.herokuapp.com&returnIDParam=idp';
   }
 
 }
